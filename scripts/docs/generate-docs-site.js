ce93d7f225f4faf4c439aaa92e8009e73e6480fc
Vadim Basko <vadim.p.basko@gmail.com>
2020-07-24 14:10:34 +0400

    docs: minor generation improvements

--- scripts/docs/generate-docs-site.js
+++ scripts/docs/generate-docs-site.js
@@ -39,6 +39,7 @@ function copyApiReference(playbook, docsSrcPath) {
     // Checkout the repo to a temporary directory
     const projectRootDir = process.cwd();
     const gitTempDir = `${docsSrcPath}/_temp`;
+    runCmdSync(`rm -rf ${gitTempDir}`);
     runCmdSync(`mkdir ${gitTempDir}`);
     process.chdir(gitTempDir);
     runCmdSync(`git clone ${repo}`);
