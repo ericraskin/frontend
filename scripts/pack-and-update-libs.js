f4aa1b4ca972f7c57cc6a2b3ec804b03a9f9e4f9
Vadim Basko <vadim.p.basko@gmail.com>
2020-08-23 14:14:01 +0400

    chore: restore CI tests for OS Win

---
+++ scripts/pack-and-update-libs.js
@@ -0,0 +1,11 @@
+const packClientLibs = require('./pack-client-libs');
+
+packClientLibs(
+  [],
+  [
+    '@cuba-platform/rest',
+    '@cuba-platform/react-core',
+    '@cuba-platform/react-ui'
+  ]
+);
+
